5308312c1486937c36e48fc69d550aeaf4563127
Zhenya <zhenya.lugovskoi95@gmail.com>
2021-01-11 19:09:43 +0200

    add bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,18 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import 'jquery'
+import '@popperjs/core'
+import 'bootstrap'
+
+import '../stylesheets/application'
+
+document.addEventListener("turbolinks:load", function () {
+	$(function () {
+		$('[data-toggler="tooltip"]').tooltip()
+		$('[data-toggler="popover"]').popover()
+	})
+})
 
 Rails.start()
 Turbolinks.start()
