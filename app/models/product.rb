f484f8f592a18620c741790379609a94e49133b7
Zhenya <zhenya.lugovskoi95@gmail.com>
2021-01-12 19:48:19 +0200

    products add

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,4 @@
 class Product < ApplicationRecord
   validates :title, :price, :description, presence: true
+  belongs_to :category
 end
--- db/migrate/20210111190715_add_category_id_to_product.rb
+++ db/migrate/20210111190715_add_category_id_to_product.rb
@@ -1,5 +1,5 @@
 class AddCategoryIdToProduct < ActiveRecord::Migration[6.1]
   def change
-    add_reference :products, :category, null: true , foreign_key: true
+    add_reference :products, :category, null: true, foreign_key: true
   end
 end
